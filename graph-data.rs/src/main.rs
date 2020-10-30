5d4ac1ef6438a0867d88b2f4eb5edc76797863c5
Vadim Rutkovsky <vrutkovs@redhat.com>
2020-10-30 11:50:57 +0100

    graph-data.rs: check_releases should return a list of scraped Releases

--- graph-data.rs/src/main.rs
+++ graph-data.rs/src/main.rs
@@ -1,10 +1,14 @@
 mod check_releases;
+mod check_signatures;
 mod verify_yaml;
+
 use anyhow::Result as Fallible;
+
+use cincinnati::Release;
 
 async fn run_all_tests() -> Fallible<()> {
     let found_versions = verify_yaml::run().await?;
-    check_releases::run(&found_versions).await?;
+    let releases: Vec<Release> = check_releases::run(&found_versions).await?;
     Ok(())
 }
 
