94438cc25e8e943949a667a99d6fa049bdd2b496
W. Trevor King <wking@tremily.us>
2021-07-23 16:08:56 -0700

    hack/stabilization-changes: Use the configured token to push
    Avoid:
    
      Branch promote-4.6.39-to-stable-4.6 set up to track remote branch master from origin.
      [promote-4.6.39-to-stable-4.6 71a3666] channels/stable-4.6: Promote 4.6.39
       1 file changed, 1 insertion(+)
      fatal: could not read Username for 'https://github.com': No such device or address
      ERROR:   failed to promote 4.6.39 to stable-4.6: Command '['git', 'push', '-u', 'origin', 'promote-4.6.39-to-stable-4.6']' returned non-zero exit status 128.
    
    I think that's Git saying "I need to authenticate this push.  Let me
    ask for credentials using the TTY.  What!?  No TTY in this container!
    Aaaah!".
    
    I spent a while looking at [1], which sounded promising, but while
    there is clear discussion on how to set up the cache helper and such,
    and how to insert creds into the cache via TTY prompts, I couldn't
    find a way to insert credentials without using a TTY.
    
    So instead, this commit constructs a remote upstream URI, inserting
    the configured GitHub token as user information [2].  And then we push
    to that URI instead of pushing to the remote.  The token will be
    visible to folks with /proc/ access, but they could already read it
    out of our environment variables.  And we keep it off the local disk.
    
    urlsplit returns a named tuple [3], and the _replace method allows
    us to slot in our token user information [4].
    
    The bulk of the new code is up before the branch checkout, so if we
    raise an exception in there, we do it without poluting the local disk
    state with an abandoned branch.
    
    [1]: https://git-scm.com/docs/gitcredentials
    [2]: https://datatracker.ietf.org/doc/html/rfc3986/#section-3.2.1
    [3]: https://docs.python.org/3/library/urllib.parse.html#urllib.parse.urlsplit
    [4]: https://docs.python.org/3/library/collections.html#collections.somenamedtuple._replace

--- hack/stabilization-changes.py
+++ hack/stabilization-changes.py
@@ -317,6 +317,16 @@ def notify(message, webhook=None):
 def promote(version, channel_name, channel_path, subject, body, github_repo, github_token, upstream_remote, upstream_branch):
     if not github_token:
         raise ValueError('cannot promote without a configured GitHub token')
+
+    process = subprocess.run(['git', 'remote', 'get-url', '--push', upstream_remote], check=True, capture_output=True, text=True)
+    upstream_uri = urllib.parse.urlsplit(process.stdout.strip())
+    if upstream_uri.scheme != 'https':
+        raise ValueError('pushing by token requires HTTPS for security, so cannot use {} scheme {}'.format(upstream_remote, upstream_uri.scheme))
+    _, have_user_info, host_info = upstream_uri.netloc.rpartition('@')
+    if have_user_info:
+        raise ValueError('remote {} configured with user info is not supported, because we expect to authenticate with the configured GitHub token'.format(upstream_remote))
+    upstream_uri_with_token = urllib.parse.urlunsplit(upstream_uri._replace(netloc='{}@{}'.format(github_token, host_info)))
+
     subprocess.run(['git', 'fetch', upstream_remote], check=True)
     branch = 'promote-{}-to-{}'.format(version, channel_name)
     try:
@@ -341,7 +351,7 @@ def promote(version, channel_name, channel_path, subject, body, github_repo, git
         yaml.safe_dump(data, f, default_flow_style=False)
     message = '{}\n\n{}\n'.format(subject, textwrap.fill(body, width=76))
     subprocess.run(['git', 'commit', '--file', '-', channel_path], check=True, encoding='utf-8', input=message)
-    subprocess.run(['git', 'push', '-u', upstream_remote, branch], check=True)
+    subprocess.run(['git', 'push', '-u', upstream_uri_with_token, branch], check=True)
 
     github_object = github.Github(github_token)
     repo = github_object.get_repo(github_repo)
