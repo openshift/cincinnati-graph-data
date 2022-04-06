d45fac659ecebddb2d749b776a20b6de32ed50b1
W. Trevor King <wking@tremily.us>
2022-04-06 15:43:55 -0700

    hack/release-feature-freeze: Script feature freeze
    So folks can call a script instead of reading docs to prepare a new
    minor release for feature candidate builds.  Like 288b5daa57
    (hack/release-ga: Automate GA steps, 2022-03-10, #1613), but for the
    earlier lifecycle event.
    
    I've left of the final:
    
      $ git add build-suggestions/* channels/*
      $ git commit
    
    but we could do that in the future if we wanted to control the commit
    message.
    
    As described in 654f0faae1 (channels/candidate-4.10: Create this
    channel, 2021-12-10, #1281), we don't want to create fast-* or later
    channels at this point, because we're still far from GA, and we don't
    want folks running 4.y.z to see a fast-4.(y+1) channel, get all
    excited, switch to that channel, and then not see any 4.(y+1).z
    targets for weeks.

---
+++ hack/release-feature-freeze.sh
@@ -0,0 +1,63 @@
+#!/bin/sh
+
+set -e
+
+MAJOR_MINOR="${1}"
+
+if test -z "${MAJOR_MINOR}"
+then
+	cat <<-EOF >&2
+		This script creates the necessary files for for a new x.y minor release feature freeze.
+		Usage:
+		  ${0} MAJOR_MINOR
+		For example:
+		  ${0} 4.10
+	EOF
+	exit 1
+fi
+
+CHANNELS="$(printf '%s\n%s' fast stable)"
+
+MAJOR="${MAJOR_MINOR%%.*}"
+MINOR="${MAJOR_MINOR##*.}"
+if test "${MAJOR_MINOR}" != "${MAJOR}.${MINOR}"
+then
+	cat <<-EOF >&2
+		Usage:
+		  ${0} MAJOR_MINOR
+		For example:
+		  ${0} 4.10
+		${MAJOR_MINOR} should have a single period.
+	EOF
+	exit 1
+fi
+
+if test "$((MINOR % 2))" -eq 0
+then
+	# 4.even get extended update support: https://access.redhat.com/support/policy/updates/openshift#ocp4_phases
+	CHANNELS="$(printf '%s\n%s' "${CHANNELS}" eus)"
+fi
+
+PREVIOUS_MINOR="$((MINOR - 1))"
+
+cat <<EOF > "build-suggestions/${MAJOR_MINOR}.yaml"
+default:
+  minor_min: ${MAJOR}.${PREVIOUS_MINOR}.0
+  minor_max: ${MAJOR}.${PREVIOUS_MINOR}.9999
+  minor_block_list: []
+  z_min: ${MAJOR_MINOR}.0-fc.0
+  z_max: ${MAJOR_MINOR}.9999
+  z_block_list: []
+EOF
+
+cat <<EOF > "channels/candidate-${MAJOR_MINOR}.yaml"
+feeder:
+  delay: PT0H
+  filter: ${MAJOR}[.](${PREVIOUS_MINOR}|${MINOR})[.][0-9].*
+  name: candidate
+name: candidate-${MAJOR_MINOR}
+versions: []
+EOF
+
+DIR="$(dirname "${0}")"
+exec "${DIR}/stabilization-changes.py"
