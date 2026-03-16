c20b01380824a6800e7c8904a7e5b0e7c650384e
W. Trevor King <wking@tremily.us>
2026-03-16 10:45:23 -0700

    hack/release-open: Adjust for 5.22 to 5.0
    5.0 isn't expected to be an EUS release, despite the even minor
    version, so drop the PREVIOUS_MINORS and '% 2' modulus handling.
    
    Also hard-code that the 4.y release that's the "previous minor" for
    5.0 is 4.22.  Point out that 5.1 is going to take more thinking, so
    exit 1 in that situation to remind us to think it over.

--- hack/release-open.sh
+++ hack/release-open.sh
@@ -30,19 +30,29 @@ then
 	exit 1
 fi
 
+PREVIOUS_MAJOR="${MAJOR}"
 PREVIOUS_MINOR="$((MINOR - 1))"
-PREVIOUS_MINORS="${PREVIOUS_MINOR}"
-if test "$((MINOR % 2))" -eq 0
-then
-	PREVIOUS_MINORS="$((MINOR - 2))|${PREVIOUS_MINORS}"
-fi
+case "${MAJOR_MINOR}" in
+5.0)
+	PREVIOUS_MAJOR=4
+	PREVIOUS_MINOR=22
+	;;
+5.1)
+	echo "Figure out what we want to do for 5.1" >&2
+	exit 1
+esac
 
-FILTER="${MAJOR}[.](${PREVIOUS_MINORS}|${MINOR})[.][0-9].*"
+if test "${MAJOR}" = "${PREVIOUS_MAJOR}"
+then
+	FILTER="${MAJOR}[.](${PREVIOUS_MINOR}|${MINOR})[.][0-9].*"
+else
+	FILTER="(${MAJOR}[.]${MINOR}|${PREVIOUS_MAJOR}[.]${PREVIOUS_MINOR})[.][0-9].*"
+fi
 
 cat <<EOF > "build-suggestions/${MAJOR_MINOR}.yaml"
 default:
-  minor_min: ${MAJOR}.${PREVIOUS_MINOR}.0-rc.0
-  minor_max: ${MAJOR}.${PREVIOUS_MINOR}.9999
+  minor_min: ${PREVIOUS_MAJOR}.${PREVIOUS_MINOR}.0-rc.0
+  minor_max: ${PREVIOUS_MAJOR}.${PREVIOUS_MINOR}.9999
   minor_block_list: []
   z_min: ${MAJOR_MINOR}.0-ec.0
   z_max: ${MAJOR_MINOR}.9999
